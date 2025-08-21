1798b3763f5b287f44e588b59c8d57dba4be7c25
Akashnagare-20 <aanagare02@gmail.com>
2025-08-21 11:24:07 +0530

    code

---
+++ JavaScript/loop.js
@@ -0,0 +1,25 @@
+//for loop//
+
+let num=4
+
+for(let i=0; i<=num; i++){
+    console.log(i);
+}
+
+
+//while loop//
+let numb=4
+
+while(i<=numb){
+    console.log(i);
+    i++;
+}
+
+//do while//
+
+let number=6;
+
+do{
+    console.log(number);
+    number++;
+} while(number<10);
---
+++ JavaScript/switchcase.js
@@ -0,0 +1,108 @@
+let day=1;
+
+switch (day){
+    case 1:
+        console.log('January');
+        break;
+    case 2:
+        console.log('February');
+        break;
+    case 3:
+        console.log('March');
+        break;
+    case 4:
+        console.log('April');
+        break;
+    case 5:
+        console.log('May');
+        break;
+    case 6:
+        console.log('June');
+        break;
+    case 7:
+        console.log('July');
+        break;
+    case 8:
+        console.log('August');
+        break;
+    case 9:
+        console.log('Septeber');
+        break;
+    case 10:
+        console.log('October');
+        break;
+    case 11:
+        console.log('November');
+        break;
+    case 12:
+        console.log('December');
+        break;
+    default:
+        console.log('Please Enter between 1 to 12');
+
+}
+
+
+
+
+
+let number=1;
+
+switch (day){
+    case 1:
+        console.log('Add');
+        break;
+    case 2:
+        console.log('Substract');
+        break;
+    case 3:
+        console.log('Multiply');
+        break;
+    case 4:
+        console.log('Division');
+        break;
+    default:
+        console.log('Please Enter between 1 to 4');
+}
+
+
+let Drink=1;
+
+switch (Drink){
+    case 1:
+        console.log('Coffee');
+        break;
+    case 2:
+        console.log('Tea');
+        break;
+    case 3:
+        console.log('Juice');
+        break;
+    case 4:
+        console.log('Water');
+        break;
+    default:
+        console.log('Please Enter between 1 to 4');
+}
+
+let letter= "a";
+
+switch (letter){
+    case "a":
+        console.log('Vowel');
+        break;
+    case "e":
+        console.log('Vowel');
+        break;
+    case "i":
+        console.log('Vowel');
+        break;
+    case "o":
+        console.log('Vowel');
+        break;
+    case "u":
+        console.log('Vowel');
+        break;
+    default:
+        console.log('Consonant');
+}
